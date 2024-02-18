be54cd6710eae82ea6b5c4a4bc035095de824f23
Shubhanshu Arya <shubhanshuarya2020@gmail.com>
2024-02-18 22:09:55 +0530

    fix

--- app/[hash]/page.tsx
+++ app/[hash]/page.tsx
@@ -3,16 +3,22 @@
 import React from 'react';
 import InvoiceForm from '@/components/invoice/Invoice';
 import { usePathname } from 'next/navigation';
+import { ConnectWallet, useAddress } from '@thirdweb-dev/react';
 
 function Page() {
   const pathname = usePathname();
-
+  const address = useAddress();
   // Check if pathname is defined before rendering InvoiceForm
   if (!pathname) {
     return null; // or any other fallback UI if you wish
   }
 
-  return <InvoiceForm status="update" code={pathname} />;
+  return (
+    <>
+      {!address && <ConnectWallet />}
+      {address && <InvoiceForm status="update" code={pathname} />}
+    </>
+  );
 }
 
 export default Page;
